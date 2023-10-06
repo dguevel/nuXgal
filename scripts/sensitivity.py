5de8ed21edd937fce7e7e4cb9f1e6c1ec2eb5733
David Guevel <guevel.david@gmail.com>
2023-10-06 08:56:50 -0500

    add chi square plots

--- scripts/sensitivity.py
+++ scripts/sensitivity.py
@@ -8,9 +8,29 @@ import csky as cy
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import histlite as hl
+import scipy.stats
 
 from KIPAC.nuXgal import Defaults
 from KIPAC.nuXgal.Likelihood import Likelihood
+from KIPAC.nuXgal import Defaults
+
+def plot_chi2_distribution(data, outputdir, ninj, ebinmin, ebinmax):
+    fig, ax = plt.subplots(dpi=300)
+    ax.hist([i[0] for i in data['chi_square'][data['n_inj'] == ninj]], bins=np.arange(0, 900, 10), density=True, label=r'Trial $\chi^2$')
+    dof = data['dof'][0]
+    print(dof)
+    chi2 = scipy.stats.chi2(df=dof)
+    x = np.arange(0, 1000)
+    ax.step(x, chi2.pdf(x), label='$\chi^2$ PDF with {:d} DoF'.format(dof))
+    ax.legend()
+    ax.set_xlim(0, 900)
+    elo = 10**Defaults.map_logE_edge[ebinmin]
+    ehi = 10**Defaults.map_logE_edge[ebinmax]
+    ax.set_title('Cross correlation fit bias: {0:.1f}-{1:.1f} TeV'.format(elo, ehi))
+    ax.set_xlabel(r'$\chi^2$')
+    fname = os.path.join(outputdir, 'unwise_chi2_dist_ebin{}.png'.format(ebinmin))
+    plt.savefig(fname, bbox_inches='tight')
+    plt.savefig(fname.replace('png', 'pdf'), bbox_inches='tight')
 
 def plot_bg_TS(data, outputdir):
     """Plot the background TS distribution."""
@@ -39,6 +59,7 @@ def plot_bg_TS(data, outputdir):
     plt.tight_layout()
     fname = os.path.join(outputdir, 'unwise_TS_dist_ebin{}.png'.format(ebin))
     plt.savefig(fname, bbox_inches='tight')
+    plt.savefig(fname.replace('png', 'pdf'), bbox_inches='tight')
     return b
 
 def load_data(files):
@@ -62,10 +83,10 @@ def plot_sensitivity(sens, disc, ns, gamma=2.0, label='template', outputdir='.',
     ax.axhline(0.9, alpha=.5, c='black')
     ax.axhline(0.5, alpha=.5, c='black')
     ax.set_title(label + ': $\gamma=' + str(gamma) + '$')
-    #ax.set_xlim(0, 1.6e-17)
     ax.legend()
     fname = os.path.join(outputdir, 'unwise_TS_sensitivity_ebin{}.png'.format(ebin))
     plt.savefig(fname, bbox_inches='tight')
+    plt.savefig(fname.replace('png', 'pdf'), bbox_inches='tight')
 
 def plot_fit_bias(df, outputdir, ebin=0):
     fig, ax = plt.subplots(dpi=100)
@@ -73,8 +94,6 @@ def plot_fit_bias(df, outputdir, ebin=0):
 
     logemin = Defaults.map_logE_edge[ebin]
     logemax = Defaults.map_logE_edge[ebin + 1]
-    title = 'unWISE-2MASS TS Distribution: {0:d}-{1:d} TeV'.format(
-        int(10**logemin), int(10**(logemax)))
     x = [np.mean(df['n_inj'][df['n_inj'] == fi]) for fi in n_inj]
     y = np.array([np.mean([i[0] for i in df['n_fit'][df['n_inj'] == fi]]) for fi in n_inj])
     yerr = np.array([np.std([i[0] for i in df['n_fit'][df['n_inj'] == fi]]) for fi in n_inj])
@@ -86,6 +105,7 @@ def plot_fit_bias(df, outputdir, ebin=0):
     ax.set_title('Cross correlation fit bias: {0:d}-{1:d} TeV'.format(int(10**ebin), int(10**(ebin+1))))
     fname = os.path.join(outputdir, 'unwise_fit_bias_ebin{}.png'.format(ebin))
     plt.savefig(fname, bbox_inches='tight')
+    plt.savefig(fname.replace('png', 'pdf'), bbox_inches='tight')
 
 
 def main():
@@ -94,8 +114,8 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('-i', '--input', nargs='+', help='Input files')
     parser.add_argument('-o', '--output', help='Output directory')
-    parser.add_argument('-ebinmin', '--ebinmin', help='Minimum energy bin', type=int)
-    parser.add_argument('-ebinmax', '--ebinmax', help='Maximum energy bin', type=int)
+    parser.add_argument('--ebinmin', help='Minimum energy bin', type=int)
+    parser.add_argument('--ebinmax', help='Maximum energy bin', type=int)
     parser.add_argument('--gamma', help='Spectral index', type=float, default=2.5)
 
     args = parser.parse_args()
@@ -103,6 +123,18 @@ def main():
     # load data
     print('Reading files...')
     df = load_data(args.input)
+
+    if not args.ebinmin and not args.ebinmax:
+        ebinmin = np.unique(df['ebinmin'])
+        ebinmax = np.unique(df['ebinmax'])
+        if len(ebinmin) != 1 or len(ebinmax) != 1:
+            raise IOError('Multiple energy bins found in input files. Please choose one.')
+        else:
+            ebinmin = ebinmin[0]
+            ebinmax = ebinmax[0]
+    else:
+        ebinmin = args.ebinmin
+        ebinmax = args.ebinmax
 
     # plot background TS distribution
     plot_bg_TS(df, args.output)
@@ -117,12 +149,16 @@ def main():
 
     plot_fit_bias(df, args.output, ebin=ebin)
 
+    plot_chi2_distribution(df, args.output, 0, ebinmin, ebinmax)
+    ninj = {0:5000, 1:5000, 2:500, 3:50}
+    plot_chi2_distribution(df, args.output, ninj, ebinmin, ebinmax)
+
     llh = Likelihood(
         'ps_v4',
         'unWISE_z=0.4',
         gamma=args.gamma,
-        Ebinmin=args.ebinmin,
-        Ebinmax=args.ebinmax,
+        Ebinmin=ebinmin,
+        Ebinmax=ebinmax,
         lmin=0)
 
     trials = {}
