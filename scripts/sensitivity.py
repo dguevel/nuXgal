77e65d0676dffdc6ae337e63d77d86c8b1b98c2f
David Guevel <guevel.david@gmail.com>
2023-10-08 18:07:24 -0500

    fix titles and fit bias plot

--- scripts/sensitivity.py
+++ scripts/sensitivity.py
@@ -25,11 +25,11 @@ def plot_chi2_distribution(data, outputdir, ninj, ebinmin, ebinmax):
     ax.step(x, chi2.pdf(x), label='$\chi^2$ PDF with {:d} DoF'.format(dof))
     ax.legend()
     ax.set_xlim(0, 900)
-    elo = 10**Defaults.map_logE_edge[ebinmin]
-    ehi = 10**Defaults.map_logE_edge[ebinmax]
-    ax.set_title('Cross correlation fit bias: {0:.1f}-{1:.1f} TeV'.format(elo, ehi))
+    elo = 10**(Defaults.map_logE_edge[ebinmin] - 3)
+    ehi = 10**(Defaults.map_logE_edge[ebinmax] - 3)
+    ax.set_title(r'$\chi^2$: {0:.1f}-{1:.1f} TeV'.format(elo, ehi))
     ax.set_xlabel(r'$\chi^2$')
-    fname = os.path.join(outputdir, 'unwise_chi2_dist_ebin{}.png'.format(ebinmin))
+    fname = os.path.join(outputdir, 'unwise_chi2_dist_ebin{}_ninj{}.png'.format(ebinmin, ninj))
     plt.savefig(fname, bbox_inches='tight')
     plt.savefig(fname.replace('png', 'pdf'), bbox_inches='tight')
 
@@ -39,8 +39,8 @@ def plot_bg_TS(data, outputdir):
     ebin = data['ebinmin'][0]
     logemin = Defaults.map_logE_edge[ebin] - 3
     logemax = Defaults.map_logE_edge[ebin + 1] - 3
-    title = 'unWISE-2MASS TS Distribution: {0:d}-{1:d} TeV'.format(
-        int(10**logemin), int(10**(logemax)))
+    title = 'unWISE-2MASS TS Distribution: {0:.1f}-{1:.1f} TeV'.format(
+        10**logemin, 10**(logemax))
 
     b = cy.dists.Chi2TSD(TS)
     fig, ax = plt.subplots(dpi=300)
@@ -93,17 +93,19 @@ def plot_fit_bias(df, outputdir, ebin=0):
     fig, ax = plt.subplots(dpi=100)
     n_inj = np.sort(np.unique(df['n_inj']))
 
-    logemin = Defaults.map_logE_edge[ebin]
-    logemax = Defaults.map_logE_edge[ebin + 1]
+    logemin = Defaults.map_logE_edge[ebin] - 3
+    logemax = Defaults.map_logE_edge[ebin + 1] - 3
     x = [np.mean(df['n_inj'][df['n_inj'] == fi]) for fi in n_inj]
-    y = np.array([np.mean([i[0] for i in df['n_fit'][df['n_inj'] == fi]]) for fi in n_inj])
-    yerr = np.array([np.std([i[0] for i in df['n_fit'][df['n_inj'] == fi]]) for fi in n_inj])
-    ax.fill_between(x, y-yerr, y+yerr, alpha=.5)
+    y = np.array([np.median([i[0] for i in df['n_fit'][df['n_inj'] == fi]]) for fi in n_inj])
+    #yerr = np.array([np.std([i[0] for i in df['n_fit'][df['n_inj'] == fi]]) for fi in n_inj])
+    #ax.fill_between(x, y-yerr, y+yerr, alpha=.5)
+    yerr = np.array([np.percentile([i[0] for i in df['n_fit'][df['n_inj'] == fi]], [32, 68]) for fi in n_inj]).T
+    ax.fill_between(x, yerr[0], yerr[1], alpha=.5)
     ax.plot(x, y)
     ax.plot(x, x, '--', c='C0')
     ax.set_xlabel('Neutrinos injected')
     ax.set_ylabel('Neutrinos fit')
-    ax.set_title('Cross correlation fit bias: {0:d}-{1:d} TeV'.format(int(10**ebin), int(10**(ebin+1))))
+    ax.set_title('Cross correlation fit bias: {0:.1f}-{1:.1f} TeV'.format(10**logemin, 10**logemax))
     fname = os.path.join(outputdir, 'unwise_fit_bias_ebin{}.png'.format(ebin))
     plt.savefig(fname, bbox_inches='tight')
     plt.savefig(fname.replace('png', 'pdf'), bbox_inches='tight')
@@ -163,6 +165,7 @@ def main():
     parser.add_argument('--ebinmin', help='Minimum energy bin', type=int)
     parser.add_argument('--ebinmax', help='Maximum energy bin', type=int)
     parser.add_argument('--gamma', help='Spectral index', type=float, default=2.5)
+    parser.add_argument('--analytic-thresholds', action='store_true', help='Use analytic thresholds')
 
     args = parser.parse_args()
 
@@ -188,17 +191,21 @@ def main():
     # calculate sensitivity and discovery potential
     b = cy.dists.Chi2TSD(df['TS'][df['n_inj'] == 0])
     n_inj = np.sort(np.unique(df['n_inj']))
-    sens = [np.mean(df['TS'][df['n_inj'] == fi] > b.median()) for fi in n_inj]
-    disc = [np.mean(df['TS'][df['n_inj'] == fi] > b.isf_nsigma(5.)) for fi in n_inj]
+    if args.analytic_thresholds:
+        sens = [np.mean(df['TS'][df['n_inj'] == fi] > 0) for fi in n_inj]
+        disc = [np.mean(df['TS'][df['n_inj'] == fi] > 25) for fi in n_inj]
+    else:
+        sens = [np.mean(df['TS'][df['n_inj'] == fi] > b.median()) for fi in n_inj]
+        disc = [np.mean(df['TS'][df['n_inj'] == fi] > b.isf_nsigma(5.)) for fi in n_inj]
     ebin = df['ebinmin'][0]
     plot_sensitivity(sens, disc, n_inj, label=r'Counts ($l>1$)', gamma=args.gamma, outputdir=args.output, ebin=ebin)
 
     plot_fit_bias(df, args.output, ebin=ebin)
 
     plot_chi2_distribution(df, args.output, 0, ebinmin, ebinmax)
-    ninj = {0:5000, 1:5000, 2:500, 3:50}
+    ninj = {0:1000, 1:1000, 2:250, 3:30}[ebinmin]
     plot_chi2_distribution(df, args.output, ninj, ebinmin, ebinmax)
-
+    return
     llh = Likelihood(
         'ps_v4',
         'unWISE_z=0.4',
